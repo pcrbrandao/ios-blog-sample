d6553e40bc934b72988fb6a2fd179212e6f3aae1
Pedro Brandao <pcrbrandao@gmail.com>
2019-12-14 19:16:55 -0300

    atualizando gitignore

---
+++ ios-blog-sample/ios-blog-sample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-blog-sample
+//
+//  Created by Pedro Brandão on 14/12/19.
+//  Copyright © 2019 Pedro Brandão. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-blog-sample/ios-blog-sampleTests/ios_blog_sampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_blog_sampleTests.swift
+//  ios-blog-sampleTests
+//
+//  Created by Pedro Brandão on 14/12/19.
+//  Copyright © 2019 Pedro Brandão. All rights reserved.
+//
+
+import XCTest
+@testable import ios_blog_sample
+
+class ios_blog_sampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
